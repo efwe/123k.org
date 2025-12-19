d58c543ac494fa7b4c01c09f72d939eb12db224a
fw <fw@123k.org>
2025-12-19 19:20:58 +0100

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,15 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule],
+  templateUrl: './app.html',
+  styleUrl: './app.sass',
+})
+export class App {
+  protected readonly title = signal('zmapz');
+}
